ebc3c79ef73641ff830d7a6a61782199c49eb11e
Gabriel Neiva <gabrielksneivagi@gmail.com>
2025-11-08 14:26:42 -0300

    refactor: remove unused output variables from outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,3 @@
 output "sns_topic_arn" {
   value = aws_sns_topic.transactions.arn
 }
-
-output "sqs_queue_url" {
-  value = aws_sqs_queue.transactions_queue.id
-}
-
-output "lambda_name" {
-  value = aws_lambda_function.consumer.function_name
-}
