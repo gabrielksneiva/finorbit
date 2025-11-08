b2e54451fb49dd9ff8974ab5c2a66f74ad0b57e9
Gabriel Neiva <gabrielksneivagi@gmail.com>
2025-11-08 14:22:54 -0300

    feat: update outputs.tf to define separate output values for withdraw and deposit SQS queues and consumer Lambda function names

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,18 @@ output "sns_topic_arn" {
   value = aws_sns_topic.transactions.arn
 }
 
-output "sqs_queue_url" {
-  value = aws_sqs_queue.transactions_queue.id
+output "withdraw_queue_url" {
+  value = aws_sqs_queue.withdraw_queue.id
 }
 
-output "lambda_name" {
-  value = aws_lambda_function.consumer.function_name
+output "deposit_queue_url" {
+  value = aws_sqs_queue.deposit_queue.id
+}
+
+output "consumer_withdraw_name" {
+  value = aws_lambda_function.consumer_withdraw.function_name
+}
+
+output "consumer_deposit_name" {
+  value = aws_lambda_function.consumer_deposit.function_name
 }
